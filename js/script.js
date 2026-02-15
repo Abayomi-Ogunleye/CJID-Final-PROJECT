8538f6f1e88c9991c1f673554d6262230f8a82e4
Abayomi <ogunleyeabayomi43@gmail.com>
2026-02-15 09:50:44 +0100

    added hamburger menu to the portfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+const navMenu = document.querySelector('.navmenu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+navMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
+
 const submit=document.getElementById('submit');
 
 submit.addEventListener('click',
